45ffe271ff68f545215b4035b896189444e7ca60
Denis Vieira <denisvieira05@gmail.com>
2017-07-10 18:21:49 -0300

    feature/#3 added more tests
    Signed-off-by: Denis Vieira <denisvieira05@gmail.com>

---
+++ cardshowviewtakenpicturesview/src/test/java/br/com/stant/libraries/cardshowviewtakenpicturesview/CardShowTakenPictureViewImagesAdapterTest.java
@@ -0,0 +1,51 @@
+package br.com.stant.libraries.cardshowviewtakenpicturesview;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by stant on 04/07/17.
+ */
+@RunWith(Enclosed.class)
+public class CardShowTakenPictureViewImagesAdapterTest {
+
+
+    public static class Describe_serviceInspectionFormFilledQuantityPercentageByPlanningQuantity {
+
+
+        CardShowTakenPictureViewImagesAdapter cardShowTakenPictureViewImagesAdapter = new CardShowTakenPictureViewImagesAdapter(M);
+
+        String imageUrl1 = "imgUrl1";
+        String imageUrl2 = "imgUrl2";
+
+    }
+
+    public static class Context_when_addPicture extends Describe_serviceInspectionFormFilledQuantityPercentageByPlanningQuantity{
+
+        @Before
+        public void add_picture(){
+            cardShowTakenPictureViewImagesAdapter.addPicture(imageUrl1);
+        }
+
+
+        @Test
+        public void It_should_plus_size_of_currentImageUrls(){
+            Integer result = cardShowTakenPictureViewImagesAdapter.getItemCount();
+            Integer expected = 1;
+
+            Assert.assertEquals(expected, result);
+        }
+
+        @Test
+        public void It_should_add_correct_picture(){
+            String result = cardShowTakenPictureViewImagesAdapter.getData().get(0);
+            String expected = imageUrl1;
+
+            Assert.assertEquals(expected,result);
+
+        }
+    }
+}
